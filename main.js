64e6fd73501b531d1be2ed6dc0cd1bee90b8ce33
wuyeelok <wuyeelok@gmail.com>
2022-03-15 10:14:42 +0800

    Old way to fetch external data

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert("Hello javascript file is main.js and css is in style.css")
+// Old way to fetch external data
+fetch("https://dog.ceo/api/breeds/list/all")
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+    }) 
