5cb10c6a96b6304316247776b6e301c8d6f39359
wuyeelok <wuyeelok@gmail.com>
2022-03-15 19:48:14 +0800

    display first images only  based on drop down

--- main.js
+++ main.js
@@ -24,10 +24,36 @@ function createBreedList(breadList) {
 async function loadByBreed(breed) {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
     const data = await response.json()
-    document.getElementsByClassName("app__slide")[0].innerHTML = `
-        ${data.message.map(function (imgUrl) {
-        return `<img src="${imgUrl}" alt="dog image" />`
-    }).join('')}
+    // console.log(data)
+    createSlideshow(data.message)
+
+    const myStr = `
+    <div
+                    class="app__slide"
+                    style="
+                        background-image: url('https://images.dog.ceo/breeds/affenpinscher/n02110627_10147.jpg');
+                    "
+                ></div>
+    `
+
+    // document.getElementsByClassName("app__slide")[0].innerHTML = `
+    //     ${data.message.map(function (imgUrl) {
+    //     return `<img src="${imgUrl}" alt="dog image" />`
+    // }).join('')}
+    // `
+}
+
+function createSlideshow(images) {
+    // console.log(images)
+
+    document.getElementById("app__slideshow").innerHTML = `
+    <div
+    class="app__slide"
+    style="
+        background-image: url('${images[0]}');
+    "
+    >
+    </div>
     `
 }
 
