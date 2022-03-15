7050b2ed1a89665e1d10885b76c79d87ec70d6ea
wuyeelok <wuyeelok@gmail.com>
2022-03-15 17:04:04 +0800

    Mock drop down

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all")
     const data = await response.json()
-    console.log(data)
+    // console.log(data)
+    createBreedList(data.message)
 }
 
 start()
+
+
+function createBreedList(breadList) {
+
+}
