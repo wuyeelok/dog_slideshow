7009aa1b8c1ff91ad49d21aeb50281e7168b7967
wuyeelok <wuyeelok@gmail.com>
2022-03-15 11:35:42 +0800

    New way of writing fetch

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// Old way to fetch external data
-fetch("https://dog.ceo/api/breeds/list/all")
-    .then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-    }) 
+// New way of writing fetch
+async function start() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json()
+    console.log(data)
+}
+
+start()
