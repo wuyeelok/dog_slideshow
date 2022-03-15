5854232710d6946b67ed4dc8ef9e6643b230ef23
wuyeelok <wuyeelok@gmail.com>
2022-03-15 17:58:26 +0800

    fetch dog images based on dropdown

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all")
     const data = await response.json()
-    console.log(data.message)
+    // console.log(data.message)
     createBreedList(data.message)
 }
 
@@ -22,5 +22,16 @@ function createBreedList(breadList) {
 
 
 function loadByBreed(breed) {
-    console.log(`breed : ${breed}`)
+    //console.log(`breed : ${breed}`)
+    fetchBreedImage(breed)
+}
+
+async function fetchBreedImage(breed) {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const data = await response.json()
+    document.getElementById("breed__image").innerHTML = `
+        ${data.message.map(function (imgUrl) {
+        return `<img src="${imgUrl}" alt="dog image" />`
+    }).join('')}
+    `
 }
